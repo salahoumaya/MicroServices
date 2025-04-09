e34eb8e3f11d147eb79a55b72410af67f00b3230
ghada-sahli <ghada.sahli@esprit.tn>
2025-04-09 19:15:34 +0200

    services pfe

---
+++ ApiGatewayPfa/src/main/java/com/esprit/microservice/apigatewaypfa/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.esprit.microservice.apigatewaypfa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // Autoriser Angular
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
